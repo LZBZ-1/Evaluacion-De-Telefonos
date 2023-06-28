9a081394590ed4ec3b20bc2232a0c1c4b3d7c6a1
LZBZ-1 <jalb200312@gmail.com>
2023-06-28 14:14:08 -0500

    update main

--- Views/PersonaVista.h
+++ Views/PersonaVista.h
@@ -5,27 +5,32 @@
 
 using namespace std;
 
-class PersonaVista {
-private:
-	UsuarioController usuarios;
-	TelefonoController telefonos;
-	UsuarioTelefonoController control;
-public:
-	PersonaVista(UsuarioController& usuarios, TelefonoController& telefonos, UsuarioTelefonoController& control) {
-		this->usuarios = usuarios;
-		this->telefonos = telefonos;
-		this->control = control;
+void agregarUsuario(const Usuario& usuario) {
+	usuarios.cargarUsuariosDesdeArchivo();
+	telefonos.cargarTelefonosDesdeArchivo();
+	control.cargarNumeros(usuarios,telefonos);
+	if (usuarios.isExist(usuario.getDni())){
+		cout << "El usuario ya existe" << endl;
+	} else {
+		usuarios.guardarUsuarioEnArchivo(usuario);
 	}
+}
 
-	void agregarUsuario(const Usuario& usuario) {
-		usuarios.isExist(usuario.getDni()) {
-			cout << "El usuario ya existe" << endl;
-		} else {
-			usuarios.guardarUsuarioEnArchivo(usuario);
-		}
+void mostrarLineas(const unsigned int dni) {
+	usuarios.cargarUsuariosDesdeArchivo();
+	telefonos.cargarTelefonosDesdeArchivo();
+	control.cargarNumeros(usuarios,telefonos);
+	if(usuarios.isExist(dni)) {
+		Usuario& usuario = usuarios.buscarUsuario(dni);
+		List<unsigned int>& telefonosEncontrado = usuario.getNumeroTelefono();
+		for(int i = 0; i < telefonosEncontrado.size(); i++ ) {
+			telefonos.mostrarTelefono(telefonos.buscarTelefono(telefonosEncontrado[i]));
+		}	
+	} else {
+		cout << "No existe el usuario" << endl;
 	}
+}
 
-	void mostrarLineas(const unsigned int dni) {
-		Usuario& usuario = usuarios.buscarUsuario(dni);	
-	}
-};
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,9 +1,101 @@
 #include <iostream>
 #include "./Views/TelefonoVista.h"
+#include "./Views/PersonaVista.h"
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
-	mostrarTitular(987654329);
-	return 0;
+int main() {
+    int opcionPrincipal;
+    int opcionSubMenu;
+
+    do {
+        cout << "----- Menu Principal -----" << endl;
+        cout << "1. TelefonoVista" << endl;
+        cout << "2. Usuarios" << endl;
+        cout << "3. Salir" << endl;
+        cout << "Ingrese su opcion: ";
+        cin >> opcionPrincipal;
+
+        switch (opcionPrincipal) {
+            case 1:
+                do {
+                    cout << endl;
+                    cout << "----- Telefonos -----" << endl;
+                    cout << "1. Mostrar Titular Linea" << endl;
+                    cout << "2. Agregar Telefono" << endl;
+                    cout << "3. Buscar Operador Telefono" << endl;
+                    cout << "4. Desactivar Linea" << endl;
+                    cout << "5. Activar Linea" << endl;
+                    cout << "6. Volver al menu principal" << endl;
+                    cout << "Ingrese su opción: ";
+                    cin >> opcionSubMenu;
+
+                    switch (opcionSubMenu) {
+                        case 1:
+                            unsigned int numero;
+                            do {
+        						cout << "Ingrese su numero ";
+        						cin >> numero;
+
+        						if (numero < 900000000 || numero > 999999999) {
+            						cout << "Número No Valida." << endl;
+        						}
+    						} while (numero < 900000000 || numero > 999999999);
+							mostrarTitular(numero);
+                            break;
+                        case 2:
+                            unsigned int numero;
+                            unsigned int dni
+                            do {
+        						cout << "Ingrese el nuevo numero";
+        						cin >> numero;
+
+        						if (numero < 900000000 || numero > 999999999) {
+            						cout << "Número No Valida." << endl;
+        						}
+    						} while (numero < 900000000 || numero > 999999999);
+    						do {
+        						cout << "Ingrese el dni";
+        						cin >> numero;
+
+        						if (numero < 900000000 || numero > 999999999) {
+            						cout << "Número No Valida." << endl;
+        						}
+    						} while (numero < 900000000 || numero > 999999999);
+    						agregarUsuario(dni);
+    						agregarTelefono(Telefono)
+                            break;
+                        case 3:
+                            cout << "Ha seleccionado la Subopción 3." << endl;
+                            break;
+                        case 4:
+                            cout << "Ha seleccionado la Subopción 4." << endl;
+                            break;
+                        case 5:
+                            cout << "Volviendo al menú principal." << endl;
+                            break;    
+                        case 6:
+                            cout << "Volviendo al menú principal." << endl;
+                            break;
+                        default:
+                            cout << "Opción inválida. Por favor, ingrese una opción válida." << endl;
+                            break;
+                    }
+                } while (opcionSubMenu != 6);
+
+                break;
+            case 2:
+                cout << "Ha seleccionado la Opción 2." << endl;
+                break;
+            case 3:
+                cout << "Saliendo del programa." << endl;
+                break;
+            default:
+                cout << "Opcion inválida. Por favor, ingrese una opción valida." << endl;
+                break;
+        }
+
+    } while (opcionPrincipal != 3);
+
+    return 0;
 }
